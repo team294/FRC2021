aac02d89c3b9ac2243e58a358bd9b2d03279c5bc
AudreyL713 <2021leea@gmail.com>
2021-01-17 15:46:42 -0800

    Added curved trajectory
    Use "Drive Trajectory Curve Relative" button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -179,9 +179,11 @@ public class RobotContainer {
     SmartDashboard.putData("ZeroGyro", new DriveZeroGyro(driveTrain, log));
     SmartDashboard.putData("ZeroEncoders", new DriveZeroEncoders(driveTrain, log));
     SmartDashboard.putData("ZeroOdometry", new DriveResetPose(0, 0, 0, driveTrain, log));
-    SmartDashboard.putData("DriveTrajectoryRelative", new DriveFollowTrajectory(CoordType.kRelative, TrajectoryTest.calcTrajectory(log), false, PIDType.kNone, driveTrain, log)
+    SmartDashboard.putData("Drive Trajectory Relative", new DriveFollowTrajectory(CoordType.kRelative, TrajectoryTest.calcTrajectory(log), false, PIDType.kNone, driveTrain, log)
         .andThen(() -> driveTrain.tankDrive(0.0, 0.0, false)));
-    SmartDashboard.putData("DriveTrajectoryAbsolute", new DriveFollowTrajectory(CoordType.kAbsolute, TrajectoryTest.calcTrajectory(log), driveTrain, log)
+    SmartDashboard.putData("Drive Trajectory Curve Relative", new DriveFollowTrajectory(CoordType.kRelative, TrajectoryCurveTest.calcTrajectory(log), false, PIDType.kNone, driveTrain, log)
+        .andThen(() -> driveTrain.tankDrive(0.0, 0.0, false)));
+    SmartDashboard.putData("Drive Trajectory Absolute", new DriveFollowTrajectory(CoordType.kAbsolute, TrajectoryTest.calcTrajectory(log), driveTrain, log)
         .andThen(() -> driveTrain.tankDrive(0.0, 0.0, false)));
 
     // auto selection widget
