bf35eb47bb2021667e3b74c403d43ee252b89873
kmflippo <46656629+kmflippo@users.noreply.github.com>
2021-02-07 00:45:32 -0800

    updated second half of slalom
    still need to check the trajectory on sim

--- src/main/java/frc/robot/utilities/TrajectoryCache.java
+++ src/main/java/frc/robot/utilities/TrajectoryCache.java
@@ -115,10 +115,11 @@ public class TrajectoryCache {
         cache[TrajectoryType.slalomMidToF.value] = calcTrajectory("Slalom Midpoint to F", 0.4, 0.4, false, 
             new Pose2d(8.382, 1.524, new Rotation2d(Math.toRadians(90.0))),
             List.of(new Translation2d(7.62, 2.3),
-                    new Translation2d(6.858, 1.8),
+                    new Translation2d(7, 1.9),
                     new Translation2d(6.604, 0.762),
                     new Translation2d(4.572, 0.762),
-                    new Translation2d(3.048, 0.889),
+                    new Translation2d(3.048, 0.762),
+                    new Translation2d(2.286, 1.5),
                     new Translation2d(1.524, 2.286)),
             new Pose2d(0.762, 2.286, new Rotation2d(Math.toRadians(180.0)))
         );
