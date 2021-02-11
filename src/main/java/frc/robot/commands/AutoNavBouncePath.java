7162f08951e566f2046756dc6305db4f218b371c
AudreyL713 <2021leea@gmail.com>
2021-02-10 20:58:40 -0800

    Fixed error in RobotContainer

--- src/main/java/frc/robot/commands/AutoNavBouncePath.java
+++ src/main/java/frc/robot/commands/AutoNavBouncePath.java
@@ -24,15 +24,15 @@ public class AutoNavBouncePath extends SequentialCommandGroup {
     // super(new FooCommand(), new BarCommand());
     addCommands(
 
-      new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, trajectoryCache.cache[TrajectoryType.bounceSToA3.value], true, false, PIDType.kWPILib, 
+      new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, trajectoryCache.cache[TrajectoryType.bounceSToA3.value], true, true, PIDType.kTalon, 
           driveTrain, log), // Might not need to come to a full stop,
                            // TODO test 2 trajectories back to back w/out stopping
 
-      new DriveFollowTrajectory(CoordType.kAbsolute, trajectoryCache.cache[TrajectoryType.bounceA3ToA6.value], true, false, PIDType.kWPILib, driveTrain, log),
+      new DriveFollowTrajectory(CoordType.kAbsolute, trajectoryCache.cache[TrajectoryType.bounceA3ToA6.value], true, true, PIDType.kTalon, driveTrain, log),
 
-      new DriveFollowTrajectory(CoordType.kAbsolute, trajectoryCache.cache[TrajectoryType.bounceA6ToA9.value], true, false, PIDType.kWPILib, driveTrain, log),
+      new DriveFollowTrajectory(CoordType.kAbsolute, trajectoryCache.cache[TrajectoryType.bounceA6ToA9.value], true, true, PIDType.kTalon, driveTrain, log),
 
-      new DriveFollowTrajectory(CoordType.kAbsolute, trajectoryCache.cache[TrajectoryType.bounceA9ToF.value], true, false, PIDType.kWPILib, driveTrain, log)
+      new DriveFollowTrajectory(CoordType.kAbsolute, trajectoryCache.cache[TrajectoryType.bounceA9ToF.value], true, true, PIDType.kTalon, driveTrain, log)
     );
   }
 }
