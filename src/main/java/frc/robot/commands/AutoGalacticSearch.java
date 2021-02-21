179ab82f4e4b46b9b797564a408befd43b656cd6
DarrenNg <34221751+Darrengn@users.noreply.github.com>
2021-02-20 21:39:35 -0800

    Chaged to work with master

--- src/main/java/frc/robot/commands/AutoGalacticSearch.java
+++ src/main/java/frc/robot/commands/AutoGalacticSearch.java
@@ -10,6 +10,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.CoordType;
 import frc.robot.Constants.SearchType;
+import frc.robot.Constants.StopType;
 import frc.robot.commands.DriveFollowTrajectory.PIDType;
 import frc.robot.subsystems.*;
 import frc.robot.utilities.*;
@@ -26,41 +27,41 @@ public class AutoGalacticSearch extends SequentialCommandGroup {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
 
-    if(path.value == 0){ //Path Red A
+    if(path == SearchType.kRedA){ //Path Red A
       addCommands(
         new IntakePistonSetPosition(true, intake, log),
         new Wait(0.4),
         new IntakeSetPercentOutput(true, intake, log),
-        new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, trajectoryCache.cache[TrajectoryType.galacticRedA.value], 
-                                    true, true, PIDType.kTalon, driveTrain, log) // follow the trajcectory
+        new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, StopType.kBrake, trajectoryCache.cache[TrajectoryType.galacticRedA.value], 
+                                    true, PIDType.kTalon, driveTrain, log) // follow the trajcectory
       );
     }
 
-    if(path.value == 1){ //Path Red B
+    if(path == SearchType.kRedB){ //Path Red B
       addCommands(
         new IntakePistonSetPosition(true, intake, log), // deploy intake
         new Wait(0.4),
         new IntakeSetPercentOutput(true, intake, log), // start intake
-        new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, trajectoryCache.cache[TrajectoryType.galacticRedB.value], 
-                                  true, true, PIDType.kWPILib, driveTrain, log) // follow the trajcectory
+        new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, StopType.kBrake, trajectoryCache.cache[TrajectoryType.galacticRedB.value], 
+                                  true, PIDType.kWPILib, driveTrain, log) // follow the trajcectory
       );
     }
 
-    if (path.value == 2){ //Path Blue A
+    if (path == SearchType.kBlueA){ //Path Blue A
       addCommands(    
         parallel(
-            new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, trajectoryCache.cache[TrajectoryType.galacticBlueA.value], 
-                                      true, true, PIDType.kTalon, driveTrain, log), // follow the trajcectory
+            new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, StopType.kBrake, trajectoryCache.cache[TrajectoryType.galacticBlueA.value], 
+                                      true,  PIDType.kTalon, driveTrain, log), // follow the trajcectory
             new IntakeSequence(intake, log) // Deploy and run intake
         )
       );
     }
 
-    if(path.value == 3){
+    if(path == SearchType.kBlueB){
       addCommands(
       parallel(
-        new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, trajectoryCache.cache[TrajectoryType.galacticBlueB.value], 
-                                  true, true, PIDType.kTalon, driveTrain, log), // follow the trajcectory
+        new DriveFollowTrajectory(CoordType.kAbsoluteResetPose, StopType.kBrake, trajectoryCache.cache[TrajectoryType.galacticBlueB.value], 
+                                  true, PIDType.kTalon, driveTrain, log), // follow the trajcectory
         new IntakeSequence(intake, log) // Deploy and run intake
       ) 
       );
