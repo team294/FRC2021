1833e764c6dc280ac36c8c5cbefb04581ccce010
Don Sawdai <dsawdai@ieee.org>
2021-03-02 22:22:23 -0800

    Removed unused import

--- src/main/java/frc/robot/utilities/AutoSelection.java
+++ src/main/java/frc/robot/utilities/AutoSelection.java
@@ -3,7 +3,6 @@ package frc.robot.utilities;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import frc.robot.commands.DriveFollowTrajectory;
-import frc.robot.commands.DriveFollowTrajectory.PIDType;
 import frc.robot.Constants.CoordType;
 import frc.robot.commands.AutoNavBouncePath;
 import frc.robot.commands.AutoOpponentTrenchPickup;
